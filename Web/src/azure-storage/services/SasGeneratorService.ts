c3b7c4ce4eea228d1d791ab9ab4dac3c780fa8bc
AlejandroI <josealejandroibarra@hotmail.com>
2021-04-14 14:01:23 -0600

    .1: Auto stash before merge of "feature-website-1.1" and "origin/feature-website-1.1"

--- Web/src/azure-storage/services/SasGeneratorService.ts
+++ Web/src/azure-storage/services/SasGeneratorService.ts
@@ -1,19 +1,32 @@
 import { Axios } from 'axios-observable';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { BlobStorageRequest } from '../types/azure-storage';
+import axios from "axios";
+import { BaseURL } from "../../constants";
 
 export class SasGeneratorService {
-  getSasToken(): Observable<BlobStorageRequest> {
-    // return Axios.get<BlobStorageRequest>(
-    //   'https://stottle-blob-storage-api.azurewebsites.net/api/GenerateSasToken'
-    // ).pipe(map(res => res.data));
-    const res$ = of (
-        {"storageUri":"https://schlafenhase.blob.core.windows.net/","storageAccessToken":"sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-05-05T08:07:18Z&st=2021-04-14T00:07:18Z&spr=https&sig=AygIZBZrMbSHS0qE7owgTWx7BnnwkQ3zyU%2FEHH6ekcU%3D"}
+    getSasToken(): Observable<BlobStorageRequest> {
+        const response = axios.get(
+            BaseURL + "/Api/AzureBlobAuth",
+            {
+                headers: {
+                    Authorization: "Bearer " + localStorage.getItem("token"),
+                },
+            }
+            ).then( (response) => {
+                const sToken = response.data;
+                console.log(sToken);
+        });
+        const res$ = of (
+            {"storageUri":"https://schlafenhase.blob.core.windows.net/","storageAccessToken":"sv=2020-02-10&ss=bfqt&srt=sco&sp=rwdlacupx&se=2021-05-05T08:07:18Z&st=2021-04-14T00:07:18Z&spr=https&sig=AygIZBZrMbSHS0qE7owgTWx7BnnwkQ3zyU%2FEHH6ekcU%3D"}
         );
-    // const res$ = of (
-    //     {"storageUri":"https://stottleblobstorage.blob.core.windows.net/","storageAccessToken":"sv=2019-02-02&ss=b&srt=sco&spr=https&st=2021-04-12T20%3A56%3A09Z&se=2021-04-12T21%3A01%3A09Z&sp=rwdlacup&sig=3SZ5JglG%2Fd%2FDIPyLzVIpC9zSPTCuzOnz3VDwBfHjOW8%3D"}
-    //   );
-    return res$
-  }
+        return res$
+    }
 }
+
+// const res$ = of (
+//     {"storageUri":"https://stottleblobstorage.blob.core.windows.net/","storageAccessToken":"sv=2019-02-02&ss=b&srt=sco&spr=https&st=2021-04-12T20%3A56%3A09Z&se=2021-04-12T21%3A01%3A09Z&sp=rwdlacup&sig=3SZ5JglG%2Fd%2FDIPyLzVIpC9zSPTCuzOnz3VDwBfHjOW8%3D"}
+//   );
+// return Axios.get<BlobStorageRequest>(
+//   'https://stottle-blob-storage-api.azurewebsites.net/api/GenerateSasToken'
+// ).pipe(map(res => res.data));
